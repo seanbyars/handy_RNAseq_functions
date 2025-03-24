2f9845eba4424147aace1bcff30b6e50ae4692ad
seanbyars <88866766+seanbyars@users.noreply.github.com>
2025-03-24 16:48:41 +1100

    Update DEG_list2word.r

--- DEG_list2word.r
+++ DEG_list2word.r
@@ -1,13 +1,14 @@
-# ----------------------------------- print DEG table to word doc
+# ----------------------------------- print DEG table to word doc for journal submission
 # this code takes a list of DEGs, cleans and prints out as a word document
 # this saves having to do this manually in Excel and Word which can take some time
 # it also formats the values (p-values, log2FC etc) so they take up less space
+# this can be handy if the DEG list(s) from an RNAseq need to be supplied in word rather than excel
 library(flextable)
 library(officer)
 library(dplyr)
 
 # Read the DEG list and simplify so it fits nicely in a word doc
-deg_data <- read.csv("~/Desktop/dge_output.csv")
+deg_data <- read.csv("dge_output.csv")
 deg_data=deg_data[,c("EnsemblId","gene_name","biotype","logFC","P.value","FDR")]
 names(deg_data)=c("Ensembl_Id","gene","biotype","log2FC","p","FDR")
 
@@ -35,5 +36,5 @@ deg_table <- flextable(deg_data) %>%
 doc <- read_docx() %>%
   body_add_flextable(deg_table)
 
-# Save to a Word file
-print(doc, target = "~/Desktop/DEG_list_as_word_doc.docx")
+# Save to a Word file - this may take a few minutes
+print(doc, target = "DEG_list_as_word_doc.docx")
